8a615dff6c50e1f5b232fc6fa1778f7eaf061b72
robbiemallett <robbiemallett@gmail.com>
2020-07-07 16:09:11 +0100

    added track_making script

---
+++ SP_LG/track_making/track_making_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+# Request minutes of wallclock time (format hours:minutes:seconds).
+#$ -l h_rt=2:00:0
+
+# Request gigabytes of RAM for each core/thread (must be an integer followed by M, G, or T)
+#$ -l mem=4G
+
+# Request gigabytes of TMPDIR space (default is 10 GB)
+#$ -l tmpfs=5G
+
+# Set the name of the job.
+#$ -N tracks
+
+# Request cores.
+#$ -pe smp 1
+
+# Set up the job array.  In this instance we have requested 10000 tasks
+# numbered 1 to 10000.
+#$ -t 1980-2017
+
+# Set the working directory to somewhere in your scratch space.
+#$ -wd /home/ucfarm0/Scratch
+
+# 8. Run the application.
+
+python3 main.py $SGE_TASK_ID s -hpc
+
